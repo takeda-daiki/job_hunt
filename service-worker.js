3d0212dba5bf8068a91b1cb031b9b7c73dca2483
takeda-daiki <puridai@akane.waseda.jp>
2026-09-21 17:26:50 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'job-conditions-shell-v1.1';
+const CACHE = 'job-conditions-shell-v1.2';
 const SHELL = ['./', './index.html', './styles.css', './app.js', './config.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
